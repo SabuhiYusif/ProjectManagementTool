5676e5ebde2a90d4d44fff32c4306806184531f2
Sabuhi Yusifov <sayusi@ttu.ee>
2019-06-26 14:14:29 +0300

    Project repository added

--- PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/domain/Project.java
+++ PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/domain/Project.java
@@ -1,4 +1,33 @@
 package com.sbhysf.ppmtool.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 }
--- PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/repositories/ProjectRepository.java
+++ PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,10 @@
 package com.sbhysf.ppmtool.repositories;
 
-public interface ProjectRepository {
+import com.sbhysf.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
 }
