9cd3bb8b03e3b5b37ab2a9c0b0cda71872b6fd1d
Sabuhi Yusifov <sayusi@ttu.ee>
2019-07-01 00:50:38 +0300

    Project Id exception handled

--- PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/services/impl/ProjectServiceImpl.java
+++ PPMToolBackEnd/src/main/java/com/sbhysf/ppmtool/services/impl/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sbhysf.ppmtool.services.impl;
 
 import com.sbhysf.ppmtool.domain.Project;
+import com.sbhysf.ppmtool.exceptions.ProjectIdException;
 import com.sbhysf.ppmtool.repositories.ProjectRepository;
 import com.sbhysf.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,11 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public Project saveOrUpdateProject(Project project) {
-
-        //Error handling
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project Id " + project.getProjectIdentifier().toUpperCase() + " not found");
+        }
     }
 }
